562646fdb76e18d0c855f4263b317c37ea5ebf3a
jda201 <132243888+jda201@users.noreply.github.com>
2023-09-03 22:38:49 -0700

    Add files via upload

---
+++ HotelReviewClassifier.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import pandas as pd
+import string
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+# Specify the dataset file directly
+dataset_file = 'tripadvisor_hotel_reviews.csv'
+
+print('start processing data')
+# Data Processing
+input_data = pd.read_csv(dataset_file)
+input_data['overall'] = input_data['overall'].astype(object)  # fix datatype error
+input_data['reviewText'] = input_data['reviewText'].astype(object)  # fix datatype error
+
+dataset = {'reviewText': input_data['reviewText'], 'overall': input_data['overall']}
+dataset = pd.DataFrame(data=dataset)
+dataset = dataset.dropna()  # ignore if any row contained NaN
+dataset = dataset[dataset['overall'] != '3']
+dataset['label'] = dataset['overall'].apply(lambda rating: +1 if str(rating) > '3' else -1)
+
+# Splitting data into training set and testing set
+X = pd.DataFrame(dataset, columns=['reviewText'])
+y = pd.DataFrame(dataset, columns=['label'])
+train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=50)
+
+# Bag of Words model
+vectorizer = CountVectorizer(token_pattern=r'\b\w+\b')
+train_vector = vectorizer.fit_transform(train_X['reviewText'])
+test_vector = vectorizer.transform(test_X['reviewText'])
+print('processing ... ok')
+print('start training model')
+# LogisticRegression model for classification problems
+clr = LogisticRegression()
+clr.fit(train_vector, train_y.values.ravel())
+scores = clr.score(test_vector, test_y)  # accuracy
+print('training ...ok')
+print('accuracy: {}%'.format(scores * 100))
+
+
+# In[1]:
+
+
+get_ipython().system('python Untitled7.py --dataset balanced_review.csv')
+
+
+# In[2]:
+
+
+get_ipython().system('python Untitled7.ipynb --dataset balanced_review.csv')
+
+
+# In[5]:
+
+
+get_ipython().system('python Untitled7.ipynb --dataset balanced_revieu.csv')
+
+
+# In[ ]:
+
+
+
+
